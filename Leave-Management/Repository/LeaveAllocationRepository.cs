7ed77428c19dd8b383e69ab32d5a0f77fbfd7f48
hafizahsannoor <hafizahsannoor@outlook.com>
2020-02-12 02:51:29 +0500

    Added Repository CRUD Operations and Added Datatables and Fontawesomes

---
+++ Leave-Management/Repository/LeaveAllocationRepository.cs
@@ -0,0 +1,53 @@
+﻿using Leave_Management.Contracts;
+using Leave_Management.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Leave_Management.Repository
+{
+    public class LeaveAllocationRepository : ILeaveAllocationRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public LeaveAllocationRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public bool Create(LeaveAllocation entity)
+        {
+            _db.leaveAllocations.Add(entity);
+            return Save();
+        }
+
+        public bool Delete(LeaveAllocation entity)
+        {
+            _db.leaveAllocations.Remove(entity);
+            return Save();
+        }
+
+        public ICollection<LeaveAllocation> FindAll()
+        {
+            var leaveAllocation = _db.leaveAllocations.ToList();
+            return leaveAllocation;
+        }
+
+        public LeaveAllocation FindById(int id)
+        {
+            var leaveAllocation = _db.leaveAllocations.Find(id);
+            return leaveAllocation;
+        }
+
+        public bool Save()
+        {
+            var changes = _db.SaveChanges();
+            return changes > 0;
+        }
+
+        public bool Update(LeaveAllocation entity)
+        {
+            _db.leaveAllocations.Update(entity);
+            return Save();
+        }
+    }
+}
